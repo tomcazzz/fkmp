676b30e10182b857374eaa912de1c850d6c8a162
tomcazzz <38794720+tomcazzz@users.noreply.github.com>
2022-02-25 19:01:45 +0100

    Update config.php
    Delete mail information

--- src/api/includes/config.php
+++ src/api/includes/config.php
@@ -10,10 +10,5 @@
 	public $password = 'FB9ZfU7x4M7PE9YS';
 	public $db = 'd02a5cc1';
 	
-	// mail
-	public $from = 'webmaster@findelkind-records.de';
-	public $to = 'tomcaz@gmx.de';
-	public $replyto = 'findelkind-records.de';
-	public $header = 'From: webmaster@findelkind-records.de';
 }
 ?>
